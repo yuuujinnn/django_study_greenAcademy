b5892eb6435b9c4fdadd74ebb9f55b7d5c405e44
yuuujinnn <yujin_0318@naver.com>
2023-06-29 17:14:59 +0900

    검색처리

--- pyweb/config/settings.py
+++ pyweb/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^eetx2qpisue%x@8g7nxmtv(z+60repknyvymwp%-d^695h_22'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
